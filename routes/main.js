263ee2ce766f1f6d33fd84f0ccfa8c1031d663a0
Roan Jones <honestly2978@gmail.com>
2025-12-02 15:40:05 +0000

    Add /weather route

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@
 const express = require("express")
 const router = express.Router();
 const redirectLogin = require('../middleware/redirectLogin');
-
+const request = require('request');
 
 // Handle our routes
 router.get('/',function(req, res, next){
@@ -11,6 +11,26 @@ router.get('/',function(req, res, next){
 
 router.get('/about',function(req, res, next){
     res.render('about.ejs')
+});
+
+router.get('/weather', function(req, res, next){
+    let apiKey = process.env.WEATHER_API_KEY;
+    let city = 'london'
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+                    
+    request(url, function (err, response, body) {
+        if(err){
+            next(err)
+        } else {
+            //res.send(body)
+            var weather = JSON.parse(body)
+            var wmsg = 'It is '+ weather.main.temp + 
+            ' degrees in '+ weather.name +
+            '! <br> The humidity now is: ' + 
+            weather.main.humidity;
+            res.send (wmsg);
+        } 
+    });
 });
 
 router.get('/logout', redirectLogin, (req, res) => {
